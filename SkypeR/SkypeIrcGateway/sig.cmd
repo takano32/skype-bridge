b3d5716288d1d783bf182de00887f4139ef0628c
TAKANO Mitsuhiro <takano32@gmail.com>
2009-06-29 22:23:19 +0900

    trivial change

--- SkypeR/SkypeIrcGateway/sig.cmd
+++ SkypeR/SkypeIrcGateway/sig.cmd
@@ -2,4 +2,6 @@ start cscript //NoLogo sig.js hc
 start cscript //NoLogo sig.js 1981
 start cscript //NoLogo sig.js 1986
 start cscript //NoLogo sig.js termtter
+start cscript //NoLogo sig.js LL
 pause
+
--- SkypeR/SkypeIrcGateway/sig.js
+++ SkypeR/SkypeIrcGateway/sig.js
@@ -1,26 +1,26 @@
 var wShell = new ActiveXObject("WScript.Shell");
 
 var args = WScript.Arguments
-var arg = '';
+	var arg = '';
 for (i = 0; i < args.length; i++) {
-    arg += args(i);
+	arg += args(i);
 }
 
 WScript.Echo(arg);
 
 do {
-    var oExec = wShell.Exec("ruby sig.rb " + arg);
-    var pid = oExec.ProcessID;
-    WScript.Echo(pid);
-    // WScript.Sleep(1000 * 10); // 10 secs
-    do{
-        WScript.Sleep(1000 * 5);
-	if (oExec.Status != 0) {
-	    break;
-	}
-    }while(true);
-    var oExecKill = wShell.Exec("TASKKILL /F /PID " + pid);
-    WScript.Sleep(1000);
+	var oExec = wShell.Exec("ruby -rrubygems sig.rb " + arg);
+	var pid = oExec.ProcessID;
+	WScript.Echo(pid);
+	// WScript.Sleep(1000 * 10); // 10 secs
+	do{
+		WScript.Sleep(1000 * 5);
+		if (oExec.Status != 0) {
+			break;
+		}
+	}while(true);
+	var oExecKill = wShell.Exec("TASKKILL /F /PID " + pid);
+	WScript.Sleep(1000);
 } while(true);
 
 // WScript.Echo(oExecKill.Status);
