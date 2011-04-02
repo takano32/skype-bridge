68c3696e4179b6b3f751f973be05c87d56582fbb
TAKANO Mitsuhiro <takano32@gmail.com>
2011-04-02 23:58:41 +0900

    example of configobj

---
+++ Skype4Py/config.py
@@ -0,0 +1,17 @@
+from configobj import ConfigObj
+
+config = ConfigObj()
+config.filename = "skype-lingr.conf"
+
+config['arakawatomonori'] = {'skype': 'hoge', 'lingr': 'arakawatomonori'}
+config['pirate'] = {'skype': 'foo', 'lingr': 'pirate'}
+
+config.write()
+
+config = ConfigObj("skype-lingr.conf")
+
+import pprint
+pp = pprint.PrettyPrinter(indent = 4)
+
+pp.pprint(config)
+print config['arakawatomonori']['lingr']
