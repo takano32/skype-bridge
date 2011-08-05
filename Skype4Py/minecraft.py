cbdd31b613d572b098acadefb43177e8d3ac6737
takano32 <mitsuhiro.takano@gree.co.jp>
2011-08-05 14:13:25 +0900

    minecraft.py: powered by minecraft.rb feat. sorah

--- Skype4Py/minecraft.py
+++ Skype4Py/minecraft.py
@@ -2,66 +2,29 @@
 # encoding: utf-8
 # vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
 
-import sys
-sys.path.append('/usr/lib/pymodules/python2.5')
-sys.path.append('/usr/lib/pymodules/python2.5/gtk-2.0')
-
-import cgi
+import urllib2
 import json
+import math
 
-import pprint
-pp = pprint.PrettyPrinter(indent = 4)
+class Minecraft():
+    def time(self, server_time):
+        hours = math.floor((((server_time / 1000.0)+8)%24))
+        minutes = math.floor((((server_time/1000.0)%1)*60))
+        seconds = math.floor((((((server_time/1000.0)%1)*60)%1)*60))
+        return "%02d:%02d:%02d" % (hours, minutes, seconds)
 
-import os
-import re
-import time
-
-os.environ['DISPLAY'] = ":32"
-os.environ['XAUTHORITY'] = "/var/www/.Xauthority"
-
-import Skype4Py
-
-from configobj import ConfigObj
-config = ConfigObj("/home/takano32/workspace/skype-lingr-bridge/Skype4Py/skype-lingr.conf")
-
-content_length = int(os.environ['CONTENT_LENGTH'])
-request_content = sys.stdin.read(content_length)
-
-from_lingr = json.JSONDecoder().decode(request_content)
-
-print "Content-Type: text/plain"
-print
-
-def handler(msg, event):
-    pass
-
-def send_message(room, text):
-    room.SendMessage(text)
-
-skype = Skype4Py.Skype()
-skype.OnMessageStatus = handler
-skype.Attach()
-
-for event in from_lingr['events']:
-    for key in config:
-        if key == 'lingr' or key == 'skype':
-            continue
-        if event['message']['room'] == config[key]['lingr']:
-            text = event['message']['text']
-            name = event['message']['nickname']
-            if re.compile(u'¹Ó.*?Àî.*?ÃÒ.*?Â§').match(name):
-                name = event['message']['speaker_id']
-            if len(name) > 16:
-                name = event['message']['speaker_id']
-            room = config[key]['skype']
-            room = skype.Chat(room)
-            for line in text.splitlines():
-                msg = '%s: %s' % (name, line)
-                send_message(room, msg)
-    print
-
-exit()
-
-# for debug
-#print pp.pformat(from_lingr)
+    def status(self, server_name):
+        response = urllib2.urlopen('http://%s:8123/up/world/world/1' % server_name).read()
+        data = json.JSONDecoder().decode(response)
+        weather = 'â˜€'
+        if data['hasStorm']:
+            weather = 'â˜‚'
+        elif data['isThundering']:
+            weather = 'âš¡'
+        time = self.time(data['servertime'])
+        return "%s [%s] %s" % (server_name, time, weather)
+        
+if __name__ == '__main__':
+    m = Minecraft()
+    print m.status('no32.tk')
 
