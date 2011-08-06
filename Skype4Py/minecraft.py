694be080848267eb60ec651ccbdf6cc1cbe8cfd2
TAKANO Mitsuhiro <takano32@gmail.com>
2011-08-06 21:20:09 +0900

    handling charset properly

--- Skype4Py/minecraft.py
+++ Skype4Py/minecraft.py
@@ -7,25 +7,38 @@ import json
 import math
 
 class Minecraft():
+    def __init__(self):
+        self.servers = dict()
+        self.servers[u'HolyGrail'] = u'holy-grail.jp'
+        self.servers[u'futoase'] = u'192.168.32.10'
+        self.servers[u'ariela']  = u'ariela.jp'
+
     def time(self, server_time):
         hours = math.floor((((server_time / 1000.0)+8)%24))
         minutes = math.floor((((server_time/1000.0)%1)*60))
         seconds = math.floor((((((server_time/1000.0)%1)*60)%1)*60))
-        return "%02d:%02d:%02d" % (hours, minutes, seconds)
+        return u"%02d:%02d:%02d" % (hours, minutes, seconds)
 
-    def status(self, server_name):
-        response = urllib2.urlopen('http://%s:8123/up/world/world/1' % server_name).read()
+    def status(self, server_name, server_addr):
+        response = urllib2.urlopen(u'http://%s:8123/up/world/world/1' % server_addr).read()
         data = json.JSONDecoder().decode(response)
-        weather = '☀'
-        if data['hasStorm']:
-            weather = '☂'
-        elif data['isThundering']:
-            weather = '⚡'
-        time = self.time(data['servertime'])
-        return "%-16s [%s] %s" % (server_name, time, weather)
+        weather = u'☀'
+        if data[u'hasStorm']:
+            weather = u'☂'
+        elif data[u'isThundering']:
+            weather = u'⚡'
+        time = self.time(data[u'servertime'])
+        return u"%-16s [%s] %s" % (server_name, time, weather)
+
+    def statuses(self):
+        result = u""
+        for name, addr in self.servers.items():
+           result += self.status(name, addr) + '\n'
+        return result
+            
+
         
 if __name__ == '__main__':
     m = Minecraft()
-    for s in ['holy-grail.jp', 'no32.tk', 'ariela.jp']:
-        print m.status(s)
+    print m.statuses()
 
