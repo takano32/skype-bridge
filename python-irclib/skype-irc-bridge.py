a1944f248d3d109173b7abc7b001e7035245be20
takano32 <takano32@gmail.com>
2011-09-08 18:58:21 +0900

    release?

--- python-irclib/skype-irc-bridge.py
+++ python-irclib/skype-irc-bridge.py
@@ -1,10 +1,8 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
-# vim: et sts=4:ts=4:sw=4
+# vim: noet sts=4:ts=4:sw=4
 # http://d.hatena.ne.jp/nishiohirokazu/20071203/1196670766
 #
-
-# /home/takano32/workspace/skype-lingr-bridge/python-irclib/skype-irc-bridge.conf
 
 import sys
 sys.path.append('/usr/lib/pymodules/python2.5')
@@ -22,21 +20,43 @@ pp = pprint.PrettyPrinter(indent = 4)
 SERVER = "irc.freenode.net"
 PORT = 6667
 NICKNAME = "skype"
-COLOR_TAG = "\x16" #reverse
 COLOR_TAG = "\x0310" #aqua
+COLOR_TAG = "\x16" #reverse
+COLOR_TAG = "" #none
 
 CHANNEL = "#sib"
 if sys.argv[1:]:
-    CHANNEL = sys.argv[1]
+	CHANNEL = sys.argv[1]
 
 import re
 from ircbot import SingleServerIRCBot
 from irclib import nm_to_n
 
+config = ConfigObj("skype-irc-bridge.conf")
+
+def skype_handler(msg, event):
+	if len(msg.Body) == 0:
+		return
+	if event == u"RECEIVED":
+		for key in config:
+			if key == 'lingr' or key == 'skype' or key == 'irc':
+				continue
+			if msg.ChatName == config[key]['skype']:
+				name = msg.Sender.FullName
+				if len(name) == 0 or len(name) > 16:
+					name = msg.Sender.Handle
+				if config[key].has_key('irc'):
+					channel = config[key]['irc']
+					for line in msg.Body.splitlines():
+						text = '%s: %s' % (name, line)
+						bot.say(channel, text.encode('utf-8'))
+
 class SkypeIRCBridge(SingleServerIRCBot):
-	def __init__(self):
-        SingleServerIRCBot.__init__(self, [(SERVER, PORT)], NICKNAME, NICKNAME)
-		self.channel = CHANNEL
+	def __init__(self, skype, room, channel = CHANNEL, server = SERVER):
+		SingleServerIRCBot.__init__(self, [(SERVER, PORT)], NICKNAME, NICKNAME)
+		self.skype = skype
+		self.room = room
+		self.channel = channel
 
 	def on_nicknameinuse(self, c, e):
 		c.nick(c.get_nickname() + "_")
@@ -44,37 +64,35 @@ class SkypeIRCBridge(SingleServerIRCBot):
 	def on_welcome(self, c, e):
 		c.join(self.channel)
 
-	def say(self, msg):
-		self.connection.privmsg(self.channel, COLOR_TAG + msg)
+	def say(self, channel, msg):
+		self.connection.privmsg(channel, COLOR_TAG + msg)
 
 	def do_command(self, c, e):
+		msg = unicode(e.arguments()[0], "utf8")
+		self.say(self.channel, msg.encode('utf-8'))
+
+	def skype_handler(self, c, e):
 		e.nick = nm_to_n(e.source())
 		msg = unicode(e.arguments()[0], "utf8")
-		self.say(msg.encode('utf-8'))
+		text = '%s: %s' % (e.nick, msg)
+		room = self.skype.Chat(self.room)
+		room.SendMessage(text)
 
 	on_pubnotice = do_command
 	on_privnotice = do_command
-	on_pubmsg = do_command
+	on_pubmsg = skype_handler
 	on_privmsg = do_command
 
-	def skype_handler(msg, event):
-		if len(msg.Body) == 0:
-			return
-		if event == u"RECEIVED":
-			config = ConfigObj("skype-lingr.conf")
-			for key in config:
-				if key == 'lingr' or key == 'skype':
-					continue
-				if msg.ChatName == config[key]['skype']:
-					name = msg.Sender.FullName
-				if len(name) == 0 or len(name) > 16:
-					name = msg.Sender.Handle
-				room = config[key]['lingr']
-				verifier = config['lingr']['verifier']
-				for line in msg.Body.splitlines():
-				    text = '%s: %s' % (name, line)
-				    send_message(room, text, verifier)
+skype = Skype4Py.Skype()
+skype.OnMessageStatus = skype_handler
+skype.Attach()
 
-bot = SkypeIRCBridge()
-bot.start()
+for key in config:
+	if key == 'lingr' or key == 'skype' or key == 'irc':
+		continue
+	if config[key].has_key('irc'):
+		channel = config[key]['irc']
+		room = config[key]['skype']
+		bot = SkypeIRCBridge(skype, room, channel)
+		bot.start()
 
