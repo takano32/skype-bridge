bf2daa64c09c65d86bdb69dffe1077132986c9bf
takano32 <takano32@gmail.com>
2011-09-09 15:09:37 +0900

    add skype-irc-bridge.sh

---
+++ python-irclib/skype-irc-bridge.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+export XAUTHORITY=/home/takano32/.Xauthority
+export DISPLAY=:64
+
+while :;
+do
+  ps a | grep '[s]kype-irc-bridge.py' | fgrep 'python' > /dev/null
+  EXISTS=$?
+  if [ $EXISTS -ne 0 ]; then
+    echo 'CRASH!!!!'
+    date
+    python skype-irc-bridge.py &
+  fi
+  sleep 60
+done
+
