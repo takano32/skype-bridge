35b24dd11f3f0c6f75a37141469c2f832509b8de
takano32 <takano32@gmail.com>
2011-10-31 14:54:45 +0900

    add {IRC,Lingr}/chat_list.py

---
+++ Skype4Py/IRC/chat_list.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
+# encoding: utf-8
+
+import Skype4Py
+import time
+import pprint
+import os
+
+os.environ['DISPLAY'] = ":64"
+pp = pprint.PrettyPrinter(indent = 4)
+
+def handler(msg, event):
+    if event == u"RECEIVED":
+        print "ChatName %s" % msg.ChatName
+        print "Body %s" % msg.Body
+        print ""
+
+def chat_list():
+    skype = Skype4Py.Skype()
+    skype.OnMessageStatus = handler
+    skype.Attach()
+    while True:
+        time.sleep(1)
+
+class MergeList:
+    pass
+
+def main():
+    chat_list()
+
+if __name__ == "__main__":
+    main()
+
---
+++ Skype4Py/Lingr/chat_list.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
+# encoding: utf-8
+
+import Skype4Py
+import time
+import pprint
+import os
+
+os.environ['DISPLAY'] = ":16"
+os.environ['DISPLAY'] = ":16"
+pp = pprint.PrettyPrinter(indent = 4)
+
+def handler(msg, event):
+    if event == u"RECEIVED":
+        # pp.pprint(msg.Sender.FullName)
+        # print ""
+        print "ChatName %s" % msg.ChatName
+        print "Body %s" % msg.Body
+        print ""
+
+def chat_list():
+    skype = Skype4Py.Skype()
+    skype.OnMessageStatus = handler
+    skype.Attach()
+    while True:
+        time.sleep(1)
+
+class MergeList:
+    pass
+
+def main():
+    chat_list()
+
+if __name__ == "__main__":
+    # merge_bot()
+    main()
+
