19d4114c997fe2408c76287a229c6e428507e947
takano32 <mitsuhiro.takano@gree.co.jp>
2011-11-25 14:13:25 +0900

    brush up supervise script for skype irc bridge

--- Skype4Py/IRC/skype-irc-bridge.sh
+++ Skype4Py/IRC/skype-irc-bridge.sh
@@ -1,17 +1,30 @@
-#!/bin/sh
+#!/bin/bash
 
 export XAUTHORITY=/home/takano32/.Xauthority
 export DISPLAY=:16
 
+BRIDGE_PATH=$(cd $(dirname $0);pwd)
+BRIDGE_PY=${BRIDGE_PATH}/skype-irc-bridge.py
+BRIDGE_CONF=$(pwd)/skype-irc-bridge.conf
+
+if [ ! -f $BRIDGE_CONF ]; then
+	echo 'doesnt exist config file'
+	exit
+fi
+
+PID=0
+
 while :;
 do
-  ps a | grep '[s]kype-irc-bridge.py' | fgrep 'python' > /dev/null
-  EXISTS=$?
-  if [ $EXISTS -ne 0 ]; then
-    echo 'CRASH!!!!'
-    date
-    sudo -u takano32 -E python skype-irc-bridge.py &
-  fi
-  sleep 5
+	ps -p $PID > /dev/null 2>&1
+	EXISTS=$?
+	if [ $EXISTS -ne 0 ]; then
+		echo 'doesnt exist process and bootup'
+		LANG=C date
+		sudo -u takano32 -E python $BRIDGE_PY &
+		PID=$!
+		trap "kill ${PID}" EXIT
+	fi
+	sleep 0.7
 done
 
