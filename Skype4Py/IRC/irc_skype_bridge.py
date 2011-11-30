89a7410a31a267bce2376002b94250d397d7018d
takano32 <takano32@gmail.com>
2011-11-30 17:15:35 +0900

    impl all but deadlocked...

--- Skype4Py/IRC/irc_skype_bridge.py
+++ Skype4Py/IRC/irc_skype_bridge.py
@@ -4,16 +4,70 @@
 # author: takano32 <tak at no32.tk>
 #
 
+from ircbot import SingleServerIRCBot
+from irclib import nm_to_n
+from configobj import ConfigObj
+from SimpleXMLRPCServer import SimpleXMLRPCServer
+
 import sys, xmlrpclib
 
-def main():
-	peer = xmlrpclib.ServerProxy('http://' + sys.argv[1])
-	s = raw_input('>>')
-	while s:
-		room = u"hogefugafoooooo"
-		peer.say(room, unicode(s))
-		s = raw_input('>>')
+CONFIG = ConfigObj("bridge.conf")
+SERVER = 'irc.freenode.net'
+PORT = 6667
+NICKNAME = 'skype2'
+
+class IrcSkypeBridge(SingleServerIRCBot):
+	def __init__(self, server = SERVER):
+		self.channel = '#takano32bot'
+		SingleServerIRCBot.__init__(self, [(SERVER, PORT)], NICKNAME, NICKNAME)
+
+		xmlrpc_host = CONFIG['skype']['xmlrpc_host']
+		xmlrpc_port = CONFIG['skype']['xmlrpc_port']
+		self.skype = xmlrpclib.ServerProxy('http://%s:%s' % (xmlrpc_host, xmlrpc_port))
+
+	def on_nicknameinuse(self, c, e):
+		c.nick(c.get_nickname() + "_")
+
+	def on_welcome(self, c, e):
+		c.join(self.channel)
+		for key in CONFIG:
+			if key == 'skype' or key == 'irc':
+				continue
+			if CONFIG[key].has_key('irc'):
+				channel = CONFIG[key]['irc']
+				c.join(channel)
+
+	def say(self, channel, msg):
+		self.connection.privmsg(channel, msg)
+		return True
+
+	def do_command(self, c, e):
+		msg = unicode(e.arguments()[0], "utf8")
+		self.say(self.channel, msg.encode('utf-8'))
+
+	def handler(self, c, e):
+		e.nick = nm_to_n(e.source())
+		msg = unicode(e.arguments()[0], "utf8")
+		text = '%s: %s' % (e.nick, msg)
+		for key in CONFIG:
+			if key == 'skype' or key == 'irc':
+				continue
+			if CONFIG[key].has_key('irc'):
+				channel = CONFIG[key]['irc']
+				if channel == e.target():
+					self.skype.say(CONFIG[key]['skype'], text)
+
+	on_pubnotice = do_command
+	on_privnotice = do_command
+	on_pubmsg = handler
+	on_privmsg = do_command
 
 if __name__ == "__main__":
-	main()
+	host = CONFIG['irc']['xmlrpc_host']
+	port = CONFIG['irc']['xmlrpc_port']
+	sv = SimpleXMLRPCServer((host, int(port)))
+	bridge = IrcSkypeBridge()
+	bridge.start()
+	sv.register_instance(IrcSkypeBridge())
+	sv.serve_forever()
 
--- Skype4Py/IRC/skype_irc_bridge.py
+++ Skype4Py/IRC/skype_irc_bridge.py
@@ -10,27 +10,54 @@ import time
 from SimpleXMLRPCServer import SimpleXMLRPCServer
 import sys, socket
 from configobj import ConfigObj
+import sys, xmlrpclib
 
-os.environ['DISPLAY'] = ":64"
-os.environ['XAUTHORITY'] = "/var/www/.Xauthority"
+os.environ['DISPLAY'] = ":16"
+os.environ['XAUTHORITY'] = "/home/takano32/.Xauthority"
 
-config = ConfigObj("skype-irc-bridge.conf")
+CONFIG = ConfigObj("bridge.conf")
 
 class SkypeIrcBridge():
+	xmlrpc_host = CONFIG['irc']['xmlrpc_host']
+	xmlrpc_port = CONFIG['irc']['xmlrpc_port']
+	irc = xmlrpclib.ServerProxy('http://%s:%s' % (xmlrpc_host, xmlrpc_port))
 	def __init__(self):
 		self.skype = Skype4Py.Skype()
 		self.start()
 
 	@staticmethod
 	def handler(msg, event):
+		if len(msg.Body) == 0:
+			return
+		if event == u"RECEIVED":
+			for key in CONFIG:
+				if key == 'skype' or key == 'irc':
+					continue
+				if CONFIG[key].has_key('skype') and msg.ChatName == CONFIG[key]['skype']:
+					name = msg.Sender.FullName
+					if len(name) == 0 or len(name) > 16:
+						name = msg.Sender.Handle
+					if CONFIG[key].has_key('irc'):
+						channel = CONFIG[key]['irc']
+						for line in msg.Body.splitlines():
+							text = '%s: %s' % (name, line)
+							print "before"
+							SkypeIrcBridge.irc.say(channel, text.encode('utf-8'))
+							print "after"
+							if WAIT != None:
+								time.sleep(WAIT)
+							else:
+								time.sleep(len(text) / 20.0)
+
+	@staticmethod
+	def inspect_handler(msg, event):
 		if event == u"RECEIVED":
 			print "ChatName %s" % msg.ChatName
 			print "Body %s" % msg.Body
 			print ""
-
+#
 	def say(self, channel, msg):
-		room = self.skype.Chat(config['arakawatomonori']['skype'])
-		#room = self.skype.Chat(channel)
+		room = self.skype.Chat(channel)
 		room.SendMessage(msg)
 		return True
 
@@ -39,7 +66,9 @@ class SkypeIrcBridge():
 		self.skype.Attach()
 
 if __name__ == "__main__":
-	sv = SimpleXMLRPCServer((socket.gethostname(), int(sys.argv[1])))
+	host = CONFIG['skype']['xmlrpc_host']
+	port = CONFIG['skype']['xmlrpc_port']
+	sv = SimpleXMLRPCServer((host, int(port)))
 	sv.register_instance(SkypeIrcBridge())
 	sv.serve_forever()
 
