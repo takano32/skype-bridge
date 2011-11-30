ab97b20745794ddab6b1ced09d3cdfd3934203bc
takano32 <mitsuhiro.takano@gree.co.jp>
2011-11-30 15:12:03 +0900

    add irc_skype_bridge.py as xmlrpc client

---
+++ Skype4Py/IRC/irc_skype_bridge.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim: noet sts=4:ts=4:sw=4
+# author: takano32 <tak at no32.tk>
+#
+
+import sys, xmlrpclib
+
+def main():
+	peer = xmlrpclib.ServerProxy('http://' + sys.argv[1])
+	s = raw_input('>>')
+	while s:
+		room = None
+		peer.say(room, s)
+		s = raw_input('>>')
+
+if __name__ == "__main__":
+	main()
+
