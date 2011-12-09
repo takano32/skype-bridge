315aa439d1a1e7cd2f7a0eb5cbfdfacdbd7cdcc8
takano32 <takano32@gmail.com>
2011-12-09 19:25:27 +0900

    fix

--- Skype4Py/Twitter/lingr_twitter.rb
+++ Skype4Py/Twitter/lingr_twitter.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
 # vim: noet sts=4:ts=4:sw=4
-# author: takano32 <tak at no32.tk>
+# author: takano32 <tak@no32 dot tk>
 #
 
 ENV['HOME'] = '/home/takano32'
@@ -24,22 +24,17 @@ from_lingr["events"].each do |event|
 	end
 end
 
-#config = Pit.get("twitter",
-#		  :require => {
-#			  'consumer_key' => 'client CONSUMER_KEY',
-#			  'consumer_secret' => 'client CONSUMER_SECRET',
-#			  'access_token' => 'oauth ACCESS_TOKEN',
-#			  'access_token_secret' => 'oauth ACCESS_TOKEN_SERCTET',
-#	  })
-#CONSUMER_KEY = config['consumer_key']
-#CONSUMER_SECRET = config['consumer_secret']
-#ACCESS_TOKEN = config['access_token']
-#ACCESS_TOKEN_SECRET = config['access_token_secret']
-
-CONSUMER_KEY = 'sp0gUKPEBP7n8WULrrAS0w'
-CONSUMER_SECRET = 'h7UU66idNRfprnYacdOktyBlmaZijy0MXbwtDHqRM'
-ACCESS_TOKEN = '5467132-LkjIS3E5it0coR0IwNCnEEjftHDf6GNMIqCs6YcuU'
-ACCESS_TOKEN_SECRET = 'WrMBFy1tMG7aRvgBqqhDWpw3NFlaKPS983PCBADMDU'
+config = Pit.get("twitter",
+		  :require => {
+			  'consumer_key' => 'client CONSUMER_KEY',
+			  'consumer_secret' => 'client CONSUMER_SECRET',
+			  'access_token' => 'oauth ACCESS_TOKEN',
+			  'access_token_secret' => 'oauth ACCESS_TOKEN_SERCTET',
+	  })
+CONSUMER_KEY = config['consumer_key']
+CONSUMER_SECRET = config['consumer_secret']
+ACCESS_TOKEN = config['access_token']
+ACCESS_TOKEN_SECRET = config['access_token_secret']
 
 consumer = OAuth::Consumer.new(
   CONSUMER_KEY,
