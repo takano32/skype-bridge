f808ea490350bf307be98654f6ceb961dd6981ca
takano32 <mitsuhiro.takano@gree.co.jp>
2011-12-21 10:05:51 +0900

    add keypair.py for bridge

---
+++ SkypeKit/IRC/keypair.py
@@ -0,0 +1,3 @@
+# You will need to replace keyFileName with a valid keypair filename
+keyFileName = '/home/takano32/opt/SkypeKit/SkypeIrcBridge.crt';
+distroRoot 	= '/home/takano32/opt/SkypeKit/skypekit-sdk';
