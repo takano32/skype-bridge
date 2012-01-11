78d6c345b35b52d15de6c85ffbea8ad6960dcbb3
takano32 <mitsuhiro.takano@gree.co.jp>
2012-01-11 11:28:02 +0900

    fix indent

--- SkypeKit/Lingr/lingr2skype.py
+++ SkypeKit/Lingr/lingr2skype.py
@@ -22,7 +22,7 @@ xmlrpc_port = config['lingr']['xmlrpc_port']
 skype_daemon = xmlrpclib.ServerProxy('http://%s:%s' % (xmlrpc_host, xmlrpc_port))
 
 if not os.environ.has_key('CONTENT_LENGTH'):
-    exit()
+	exit()
 
 content_length = int(os.environ['CONTENT_LENGTH'])
 request_content = sys.stdin.read(content_length)
@@ -33,28 +33,28 @@ print "Content-Type: text/plain"
 print
 
 if not from_lingr.has_key('events'):
-    print
-    exit()
+	print
+	exit()
 
 for event in from_lingr['events']:
-    if not event.has_key('message'):
-        continue
-    for key in config:
-        if key == 'lingr' or key == 'skype' or key == 'irc':
-            continue
-        if not config[key].has_key('lingr'): continue
-        if not config[key].has_key('skype'): continue
-        if event['message']['room'] == config[key]['lingr']:
-            text = event['message']['text']
-            name = event['message']['nickname']
-            if re.compile(u'荒.*?川.*?智.*?則').match(name):
-                name = event['message']['speaker_id']
-            if len(name) > 16:
-                name = event['message']['speaker_id']
-            room = config[key]['skype']
-            for line in text.splitlines():
-                msg = '%s: %s' % (name, line)
-                skype_daemon.send_message(room, msg)
+	if not event.has_key('message'):
+		continue
+	for key in config:
+		if key == 'lingr' or key == 'skype' or key == 'irc':
+			continue
+		if not config[key].has_key('lingr'): continue
+		if not config[key].has_key('skype'): continue
+		if event['message']['room'] == config[key]['lingr']:
+			text = event['message']['text']
+			name = event['message']['nickname']
+			if re.compile(u'荒.*?川.*?智.*?則').match(name):
+				name = event['message']['speaker_id']
+			if len(name) > 16:
+				name = event['message']['speaker_id']
+			room = config[key]['skype']
+			for line in text.splitlines():
+				msg = '%s: %s' % (name, line)
+				skype_daemon.send_message(room, msg)
 print
 exit()
 # for debug
