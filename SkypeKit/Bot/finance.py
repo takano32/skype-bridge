643e1175e54470082424539a95586b9934b841fc
takano32 <takano32@gmail.com>
2012-01-13 13:54:49 +0900

    add some sompanies and indent w/ fixing unit

--- SkypeKit/Bot/finance.py
+++ SkypeKit/Bot/finance.py
@@ -4,7 +4,20 @@
 # author: takano32 <tak@no32 dot tk>
 #
 
-codes = ['NASDAQ:AKAM', 'NASDAQ:GOOG', 'NASDAQ:AAPL', 'TYO:3632', 'TYO:2432', 'TYO:3715', 'TYO:7733']
+codes = [
+		'NASDAQ:AKAM', # Akamai
+		'NASDAQ:GOOG', # Google
+		'NASDAQ:AAPL', # Apple
+		'NASDAQ:YHOO', # Yahoo!
+		'TYO:4689',    # Yahoo! Japan
+		'TYO:3632',    # GREE
+		'TYO:2432',    # DeNA
+		'TYO:2121',    # mixi
+		'TYO:4751',    # CyberAgent
+		'TYO:3715',    # Dwango
+		'TYO:7733',    # OLYMPUS
+		'TYO:6501',    # Hitachi
+		]
 
 import urllib2
 from BeautifulSoup import BeautifulSoup
@@ -23,13 +36,19 @@ DAEMON = xmlrpclib.ServerProxy('http://%s:%s' % (xmlrpc_host, xmlrpc_port))
 
 opener = urllib2.build_opener()
 
+base_url = 'http://www.google.com/finance?q='
+texts = [base_url]
 for code in codes:
-	html = opener.open('http://www.google.com/finance?q=' + code).read()
+	html = opener.open(base_url + code).read()
 	soup = BeautifulSoup(html)
-	com = soup.find('h3').text.replace('&nbsp;', '')
+	com = soup.find('h3').text.replace('&nbsp;', '').split()[:1][0]
 	l  = soup.find(id = re.compile('ref_[0-9]+_l')).text
+	unit = 'JPY'
+	if code.startswith('NASDAQ'): unit = 'USD'
 	c  = soup.find(id = re.compile('ref_[0-9]+_c')).text
 	cp = soup.find(id = re.compile('ref_[0-9]+_cp')).text
-	text = "[%s] %12s %12s JPY w/ %s %s" % (code, com, l, c, cp)
-	DAEMON.send_message(ROOM, text)
+	texts.append("[%-12s] %-16s %12s %s w/ %-12s %s" % (code, com, l, unit, c, cp) )
+
+text = "\n".join(texts)
+DAEMON.send_message(ROOM, text)
 
